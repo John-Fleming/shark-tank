f76aaf9682dfa813c9cd303d630958720a88eb86
John Fleming <johndeclanfleming@gmail.com>
2020-02-29 10:33:52 -0600

    finished tankBuilder function

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -1,8 +1,19 @@
 import utils from '../helpers/utils.js';
-import people from '../helpers/data/personData.js';
+import personData from '../helpers/data/personData.js';
 
 const tankBuilder = () => {
-    let domString = 'help 2.0';
+    const allAlivePeople = personData.getAlivePeople();
+    let domString = '';
+    domString += '<div class="card">';
+    domString +=   '<div class="card-header">Shark Tank</div>';
+    domString +=   '<ul class="list-group list-group-flush">';
+
+    allAlivePeople.forEach((person) => {
+        domString +=     `<li class="list-group-item">${person.name}</li>`;
+    });
+
+    domString +=   '</ul>';
+    domString += '</div>';
     utils.printToDom('tank', domString);
 };
 
