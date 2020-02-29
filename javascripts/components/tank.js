10a273cdfbe936b8393cf7687a7741cbeffc9c2e
John Fleming <johndeclanfleming@gmail.com>
2020-02-29 10:18:25 -0600

    refactored printToDom with jQuery

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -2,7 +2,7 @@ import utils from '../helpers/utils.js';
 import people from '../helpers/data/personData.js';
 
 const tankBuilder = () => {
-    let domString = 'help';
+    let domString = 'help 2.0';
     utils.printToDom('tank', domString);
 };
 
